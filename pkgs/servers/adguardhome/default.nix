9e98f1b46dbb5936c56551db5f44e07e528cda91
iagocq <18238046+iagocq@users.noreply.github.com>
2021-05-22 09:09:35 -0300

    adguardhome: 0.105.2 -> 0.106.3

--- pkgs/servers/adguardhome/default.nix
+++ pkgs/servers/adguardhome/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "adguardhome";
-  version = "0.105.2";
+  version = "0.106.3";
 
   src = fetchurl {
     url = "https://github.com/AdguardTeam/AdGuardHome/releases/download/v${version}/AdGuardHome_linux_amd64.tar.gz";
-    sha256 = "1gpaqyczidsy7h05g318zc83swvwninidddlmlq3hgs8s7ibk2cb";
+    sha256 = "sha256-qJMymTxmoPlIhuJD6zFBWWwzz+CFx+9+MOrRiFtA4IY=";
   };
 
   installPhase = ''
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/AdguardTeam/AdGuardHome";
     description = "Network-wide ads & trackers blocking DNS server";
     platforms = platforms.linux;
-    maintainers = with maintainers; [ numkem ];
+    maintainers = with maintainers; [ numkem iagoq ];
     license = licenses.gpl3;
   };
 }
